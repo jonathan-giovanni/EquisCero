8c50d324548e640e56ad6fa64741350f4e391657
jonathancplusplus <hv12i04001@usonsonate.edu.sv>
2017-04-04 17:07:50 -0600

    Proyecto subido

---
+++ Clase2/src/clase2/Clase2.java
@@ -0,0 +1,13 @@
+
+package clase2;
+
+import GUI.frmEquisCero;
+import GUI.frmPrincipal;
+
+public class Clase2 {
+    public static void main(String[] args) {
+        frmEquisCero principal = new frmEquisCero();
+        principal.setVisible(true);
+    }
+    
+}
